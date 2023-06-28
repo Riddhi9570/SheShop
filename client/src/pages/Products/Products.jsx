df80b9c77b990274a61865584d2c6d969bf0a715
Riddhi Bhagat <riddhi0bhagat@gmail.com>
2023-06-28 21:51:21 +0530

    maxprice changed

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -7,7 +7,7 @@ import "./Products.scss";
 
 const Products = () => {
   const catId = parseInt(useParams().id);
-  const [maxPrice, setMaxPrice] = useState(1000);
+  const [maxPrice, setMaxPrice] = useState(10000);
   const [sort, setSort] = useState(null);
   const [selectedSubCats, setSelectedSubCats] = useState([]);
 
@@ -50,7 +50,7 @@ const Products = () => {
             <input
               type="range"
               min={0}
-              max={1000}
+              max={10000}
               onChange={(e) => setMaxPrice(e.target.value)}
             />
             <span>{maxPrice}</span>
