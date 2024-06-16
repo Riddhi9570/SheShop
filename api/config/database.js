1246ccdd4036d861aeb2e99c9686ecc6e8875222
Riddhi Bhagat <riddhi0bhagat@gmail.com>
2024-06-16 22:43:11 +0530

    Updated and configured dependencies

--- api/config/database.js
+++ api/config/database.js
@@ -1,17 +1,28 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', '5432'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      schema: env('DATABASE_SCHEMA'), // Not required
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
     },
     debug: false,
   },
+});*/
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+  },
 });
+
 
 /* const path = require('path');
 
